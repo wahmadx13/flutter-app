84ec91506bd0c9b07c8d089415d70209d6b770b3
Waseem <wasim_ahmad2003@yahoo.com>
2024-07-30 16:04:46 +0500

    edited the verify email view text

--- lib/views/verify_email.dart
+++ lib/views/verify_email.dart
@@ -18,7 +18,10 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
       ),
       body: Column(
         children: [
-          const Text('Please verify your email address: '),
+          const Text(
+              "We've sent you an email verification. Please open it to verify your account"),
+          const Text(
+              "If you haven't received a verification email yet, press the button below"),
           TextButton(
               onPressed: () async {
                 final user = FirebaseAuth.instance.currentUser;
