c8aa02d1bfd52ff5aa19cf997549d5ea8df87e20
Waseem <wasim_ahmad2003@yahoo.com>
2024-08-04 20:08:34 +0500

    added schema for user

---
+++ lib/services/crud/notes_services.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/foundation.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart'
+    show getApplicationCacheDirectory;
+import 'package:path/path.dart';
+
+@immutable
+class DatabaseUser {
+  final int id;
+  final String email;
+  const DatabaseUser({
+    required this.id,
+    required this.email,
+  });
+
+  DatabaseUser.fromRow(Map<String, Object?> map)
+      : id = map[idColumn] as int,
+        email = map[emailColumn] as String;
+
+  @override
+  String toString() => 'Person, ID = $id, email = $email';
+
+  @override
+  bool operator ==(covariant DatabaseUser other) => id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
+}
+
+const idColumn = "id";
+const emailColumn = "email";
